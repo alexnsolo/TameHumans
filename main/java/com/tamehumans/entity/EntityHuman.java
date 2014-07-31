366af535179a325126ecc619cea95f1dabb6b6b2
Alex Solo <alex.n.solo@gmail.com>
2014-07-30 22:16:18 -0500

    humans can be tamed with gold nuggets!

--- main/java/com/tamehumans/entity/EntityHuman.java
+++ main/java/com/tamehumans/entity/EntityHuman.java
@@ -1,11 +1,15 @@
 package com.tamehumans.entity;
 
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIFollowOwner;
 import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.world.World;
 
 public class EntityHuman extends EntityTameable {
@@ -24,7 +28,49 @@ public class EntityHuman extends EntityTameable {
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20.0D);
         this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.20000000298023224D);
+    }
+
+    @Override
+    public boolean isAIEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean interact(EntityPlayer player) {
+        ItemStack playerItem = player.inventory.getCurrentItem();
+
+        if (this.isTamed()) {
+
+        }
+        else if (playerItem != null && playerItem.getItem() == Items.gold_nugget) {
+            if (!player.capabilities.isCreativeMode) {
+                --playerItem.stackSize;
+            }
+
+            if (playerItem.stackSize <= 0) {
+                player.inventory.setInventorySlotContents(player.inventory.currentItem, (ItemStack)null);
+            }
+
+            if (!this.worldObj.isRemote) {
+                if (this.rand.nextInt(10) == 0) {  // 1 in 10 chance to tame
+                    this.setTamed(true);
+                    this.setPathToEntity((PathEntity) null);
+                    this.setAttackTarget((EntityLivingBase) null);
+                    this.func_152115_b(player.getUniqueID().toString()); // not sure what this does. er...
+                    this.playTameEffect(true);
+                    this.worldObj.setEntityState(this, (byte)7);
+                }
+                else {
+                    this.playTameEffect(false);
+                    this.worldObj.setEntityState(this, (byte)6);
+                }
+            }
+
+            return true;
+        }
+
+        return super.interact(player);
     }
 }
