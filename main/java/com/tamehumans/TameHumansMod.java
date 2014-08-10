89fbf80a0e98f52fc7aac9206e19ad62f628e9a0
Alex Solo <alex.n.solo@gmail.com>
2014-08-10 00:10:40 -0500

    v 0.0.3

--- main/java/com/tamehumans/TameHumansMod.java
+++ main/java/com/tamehumans/TameHumansMod.java
@@ -19,7 +19,7 @@ import java.awt.*;
 public class TameHumansMod {
 
     public static final String MODID = "tamehumans";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
 
     @Mod.Instance(value = "mod")
     public static TameHumansMod instance;
